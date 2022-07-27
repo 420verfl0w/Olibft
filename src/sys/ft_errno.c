53420cc6e26ccaf908d3142a635c61d969ba94f4
Malo David <maldavid@3E3.42angouleme.fr>
2022-07-27 16:12:21 +0200

    fixing push issue

--- src/sys/ft_errno.c
+++ src/sys/ft_errno.c
@@ -6,19 +6,24 @@
 /*   By: maldavid <kbz_8.dev@akel-engine.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 14:51:48 by maldavid          #+#    #+#             */
-/*   Updated: 2022/07/27 15:49:09 by maldavid         ###   ########.fr       */
+/*   Updated: 2022/07/27 16:07:58 by maldavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef __STDC_NO_ATOMICS__
+#ifndef __STDC_NO_ATOMICS__
 
-static int			g_errno = 0;
+static _Atomic int	g_errno = 0;
+
+_Atomic int	*__ft_bits_get_errno_location(void)
+{
+	return (&g_errno);
+}
 #else
 
-_Atomic static int	g_errno = 0;
-#endif
+static int			g_errno = 0;
 
 int	*__ft_bits_get_errno_location(void)
 {
 	return (&g_errno);
 }
+#endif
