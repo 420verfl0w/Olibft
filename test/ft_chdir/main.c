5182beb888d9ad00c664d294b241fd4184ff693e
Unam3dd <skyrim.gaming16@gmail.com>
2022-07-27 04:14:07 +0200

    update

--- test/ft_chdir/main.c
+++ test/ft_chdir/main.c
@@ -6,7 +6,7 @@
 /*   By: sam <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/27 04:10:48 by sam               #+#    #+#             */
-/*   Updated: 2022/07/27 04:12:20 by sam              ###   ########.fr       */
+/*   Updated: 2022/07/27 04:13:59 by sam              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 #include "libft.h"
 #include <unistd.h>
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	char	path[0x100];
-	
+
 	if (ac < 2)
 		return (1);
 	ft_chdir(av[1]);
